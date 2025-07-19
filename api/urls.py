b3136af75fb6d53e6c8dcb53254165d1342d6a8a
JCRG <juancarlosrg1999@gmail.com>
2025-07-19 21:18:30 +0000

    refactor: remove v1 ref

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("v1/extract-entities/", views.extract_entities, name="extract_entities"),
+    path("extract-entities/", views.extract_entities, name="extract_entities"),
     path("healthcheck/", views.health_check, name="health_check"),
 ]
